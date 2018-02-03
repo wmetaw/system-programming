13f4a860cd251175bbadad3751480c88e5c9f356
meta <lio114514@gmail.com>
2018-02-03 18:32:12 +0900

    ioreader.go

---
+++ ioreader.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+	"io"
+	"fmt"
+)
+
+func main() {
+	for {
+		buffer := make([]byte, 5)
+		size, err := os.Stdin.Read(buffer)
+		if err == io.EOF {
+			fmt.Println("EOF")
+			break
+		}
+		fmt.Printf("size=%d input='%s'\n", size, string(buffer))
+	}
+}
