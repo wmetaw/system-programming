7ab06353b3bb94bcf40869f5eeebb1b54b67d798
Ryo Takashima <r.takashima@miniascape.co.jp>
2018-02-04 20:36:33 +0900

    gzip_client.go

---
+++ gzip_client.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"strings"
+)
+
+func main() {
+	sendMessages := []string{"HOGEHOGE", "FUGAFUGA", "PIYOPIYO"}
+
+	current := 0
+	var conn net.Conn = nil
+
+	// リトライ用にループで全体を囲む
+	for {
+		var err error
+
+		// まだコネクションを張ってない or リトライ時
+		if conn == nil {
+			conn, err = net.Dial("tcp", "localhost:8888")
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Access: %v\n", current)
+		}
+
+		// ポストで文字列を送るリクエストを作成
+		request, err := http.NewRequest("POST", "http://localhost:8888", strings.NewReader(sendMessages[current]))
+		if err != nil {
+			panic(err)
+		}
+		request.Header.Set("Accept-Encoding", "gzip")
+		err = request.Write(conn)
+		if err != nil {
+			panic(err)
+		}
+
+		// サーバーから読み込む。タイムアウトはここでエラーになるのでリトライ
+		response, err := http.ReadResponse(bufio.NewReader(conn), request)
+		if err != nil {
+			fmt.Println("Retry")
+			conn = nil
+			continue
+		}
+
+		// 結果を表示
+		dump, err := httputil.DumpResponse(response, false)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+
+		defer response.Body.Close()
+		if response.Header.Get("Content-Encoding") == "gzip" {
+			reader, err := gzip.NewReader(response.Body)
+			if err != nil {
+				panic(err)
+			}
+			io.Copy(os.Stdout, reader)
+			reader.Close()
+		} else {
+			io.Copy(os.Stdout, response.Body)
+		}
+
+		// 全部送信していれば終了
+		current++
+		if current == len(sendMessages) {
+			break
+		}
+	}
+}
